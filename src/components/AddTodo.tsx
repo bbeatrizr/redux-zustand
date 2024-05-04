13eb4a18526424428fac6c55c055de4a4928e6c6
Beatriz Ramalho <biarl.ramalhorl@gmail.com>
2024-05-04 15:04:09 -0300

    fix: 🐛 adicionando actions

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,10 +1,16 @@
 import { FormEvent, useState } from "react";
+import { useDispatch } from "react-redux";
+import { add } from "../store";
 
 export function AddTodo() {
   const [newTodo, setNewTodo] = useState("");
+  const dispatch = useDispatch();
 
   function handleNewTodo(e: FormEvent) {
     e.preventDefault();
+    dispatch(add({ newTodo }));
+
+    setNewTodo('')
   }
 
   return (
