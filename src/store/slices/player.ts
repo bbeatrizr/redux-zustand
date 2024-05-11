f1038fa71cd9f3fa80959e4d9fc80784034d401e
Beatriz Ramalho <biarl.ramalhorl@gmail.com>
2024-05-11 15:31:28 -0300

    fix: 🐛 cria testes unitarios

--- src/store/slices/player.ts
+++ src/store/slices/player.ts
@@ -1,7 +1,7 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { useAppSelector } from "..";
 
-const playerSlice = createSlice({
+export const playerSlice = createSlice({
   name: "player",
   initialState: {
     course: {
